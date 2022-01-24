cee381d53aa326a733f24bd28ddea25a6d7a44d4
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-24 18:09:42 -0500

    small change

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -19,7 +19,6 @@ const SearchBooks = () => {
   const [saveBook, {error}] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
